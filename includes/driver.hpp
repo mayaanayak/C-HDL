8354ff56b50f3c8555bf5a64830e10098b3e326a
Maya Nayak <mayan3@illinois.edu>
2021-11-16 02:52:51 -0600

    Add Add() and helper functions

--- includes/driver.hpp
+++ includes/driver.hpp
@@ -5,5 +5,10 @@
 using namespace std;
 
 enum command{ HELP, SAVE, LOAD, ADD, WIRE, UNWIRE, DELETE, LIST, UNKNOWN };
+//and, or, not, nor, nand, xor, register, monitor
 
 command GetCommand(const string &argument);
+
+void Add(std::string& first_arg, std::string& second_arg, std::string& third_arg);
+bool IsInMap(std::string& extended_module, std::string& name);
+void AddToMap(std::string& extended_module, std::string& name);
--- src/driver.cc
+++ src/driver.cc
@@ -5,20 +5,23 @@
 #include <string>
 #include <sstream>
 #include <map>
+#include <stdexcept>
+#include <algorithm>
 using namespace std;
 
 static string progTitle = "  _____  __    __    __ _____  __ \n / ___/_/ /___/ /_  / // / _ \\/ / \n/ /__/_  __/_  __/ / _  / // / /__\n\\___/ /_/   /_/   /_//_/____/____/";
 static string helpStringA = "help - prints this\nexit - terminates program\nsave <filename> - saves schematic to local directory\nload <filename> - loads schematic from local directory (clears any unsaved work!)";
 static string helpStringB = "\n\nadd <module> <name> - adds a module with the given name\ndelete <name> - removes a module with the given name\nwire <src> <dest> - connects source module to destination module\nunwire <src> <dest> - removes connection from source module to destination module, if one exists\nlist [registers/modules/monitors/all] - lists the created modules";
+
+map<string, Component*> register_map;
+map<string, Component*> schematic_map;
+map<string, Component*> monitor_map;
+
 int main(){
     cout << progTitle << endl << endl;
     cout << "Type 'help' for a list of commands. Type 'exit' to quit." << endl;
 
     //Handle autoload schematic
-
-    map<string, Component*> registerMap;
-    map<string, Component*> schematicMap;
-    map<string, Component*> monitorMap;
 
     while (true){
         string command;
@@ -88,4 +91,53 @@ command GetCommand(const string &argument) {
         return DELETE;
     }
     return UNKNOWN;
+}
+
+void Add(std::string& first_arg, std::string& second_arg, std::string& third_arg) {
+    if (first_arg != "add") throw std::invalid_argument("First argument should be 'add'");
+    std::vector<std::string> extended_modules {"and", "or", "not", "nor", "nand", "xor", "register", "monitor"};
+    if (std::find(extended_modules.begin(), extended_modules.end(), second_arg) == extended_modules.end()) {
+        throw std::invalid_argument("Second argument should be a module");
+    }
+    if (!IsInMap(second_arg, third_arg)) {
+        AddToMap(second_arg, third_arg);
+    }
+}
+
+bool IsInMap(std::string& extended_module, std::string& name) {
+    if (extended_module == "register") {
+        return !(register_map.find(name) == register_map.end());
+    }
+    if (extended_module == "monitor") {
+        return !(monitor_map.find(name) == monitor_map.end());
+    }
+    return !(schematic_map.find(name) == schematic_map.end());
+}
+
+void AddToMap(std::string& extended_module, std::string& name) {
+    if (extended_module == "register") {
+        auto* to_add = new Register();
+        register_map[name] = to_add;
+        return;
+    }
+    if (extended_module == "monitor") {
+        auto* to_add = new Monitor();
+        monitor_map[name] = to_add;
+        return;
+    }
+    Component* to_add;
+    if (extended_module == "and") {
+        to_add = new AndGate();
+    } else if (extended_module == "or") {
+        to_add = new OrGate();
+    } else if (extended_module == "not") {
+        to_add = new NotGate();
+    } else if (extended_module == "nor") {
+        to_add = new NorGate();
+    } else if (extended_module == "nand") {
+        to_add = new NandGate();
+    } else {
+        to_add = new XorGate();
+    }
+    schematic_map[name] = to_add;
 }
