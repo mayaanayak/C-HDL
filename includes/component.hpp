4718e8b2257e090280e7915847be7aceeec6bd10
Nicholas Park <kyuhahp2@@illinois.edu>
2021-11-15 23:02:29 -0600

    Syntax fixed

--- includes/component.hpp
+++ includes/component.hpp
@@ -3,18 +3,18 @@
 
 using namespace std;
 
-class Component{
+class Component {
     
     public:
-    Component(){
-        state_=false;
-        evaluate_count_=0;
-        max_inputs_=0;
+    Component() {
+        state_ = false;
+        evaluate_count_ = 0;
+        max_inputs_ = 0;
     }
     void virtual Evaluate() = 0;
-    bool GetState(){return state_;}
-    void AttachInput(Component* device){
-        if (max_inputs_==0 || inputs_.size()<max_inputs_){
+    bool GetState() { return state_; }
+    void AttachInput(Component* device) {
+        if (max_inputs_ == 0 || inputs_.size() < max_inputs_) {
             inputs_.push_back(device);
             device->outputs_.push_back(this);
         }
@@ -27,17 +27,17 @@ class Component{
     size_t max_inputs_;
 };
 
-class AndGate : public Component{
+class AndGate : public Component {
     public:
-    AndGate(){
-        max_inputs_=0;
-        state_=false;
-        evaluate_count_=0;
+    AndGate() {
+        max_inputs_ = 0;
+        state_ = false;
+        evaluate_count_ = 0;
     }
-    void Evaluate() override{
+    void Evaluate() override {
         if (inputs_.empty()) return;
         evaluate_count_++;
-        if (evaluate_count_==inputs_.size()){
+        if (evaluate_count_ == inputs_.size()){
             state_ = true;
             for (Component* input : inputs_) {
                 if (!input->GetState()) {
@@ -45,27 +45,27 @@ class AndGate : public Component{
                     break;
                 }
             }
-            for (size_t i=0; i<outputs_.size(); i++){
+            for (size_t i = 0; i < outputs_.size(); i++) {
                 outputs_[i]->Evaluate();
             }
-            evaluate_count_=0;
+            evaluate_count_ = 0;
         }
 
     }
 
 };
 
-class OrGate : public Component{
+class OrGate : public Component {
     public:
-    OrGate(){
-        max_inputs_=0;
-        state_=false;
-        evaluate_count_=0;
+    OrGate() {
+        max_inputs_ = 0;
+        state_ = false;
+        evaluate_count_ = 0;
     }
-    void Evaluate() override{
+    void Evaluate() override {
         if (inputs_.empty()) return;
         evaluate_count_++;
-        if (evaluate_count_==inputs_.size()){
+        if (evaluate_count_ == inputs_.size()) {
             state_ = false;
             for (Component* input : inputs_) {
                 if (input->GetState()) {
@@ -73,47 +73,47 @@ class OrGate : public Component{
                     break;
                 }
             }
-            for (size_t i=0; i<outputs_.size(); i++){
+            for (size_t i = 0; i < outputs_.size(); i++) {
                 outputs_[i]->Evaluate();
             }
-            evaluate_count_=0;
+            evaluate_count_ = 0;
         }
     }
 };
 
-class NotGate : public Component{
+class NotGate : public Component {
     public:
-    NotGate(){
-        max_inputs_=1;
-        state_=false;
-        evaluate_count_=0;
+    NotGate() {
+        max_inputs_ = 1;
+        state_ = false;
+        evaluate_count_ = 0;
     }
-    void Evaluate() override{
+    void Evaluate() override {
         if (inputs_.empty()) return;
         evaluate_count_++;
-        if (evaluate_count_==inputs_.size()){
+        if (evaluate_count_ == inputs_.size()) {
             state_ = !(inputs_[0]->GetState());
-            for (size_t i=0; i<outputs_.size(); i++){
+            for (size_t i = 0; i < outputs_.size(); i++) {
                 outputs_[i]->Evaluate();
             }
-            evaluate_count_=0;
+            evaluate_count_ = 0;
         }
     }
 };
 
-class Register : public Component{
+class Register : public Component {
     public:
-        Register(){
-            max_inputs_=0;
-            state_=false;
-            evaluate_count_=0;
+        Register() {
+            max_inputs_ = 0;
+            state_ = false;
+            evaluate_count_ = 0;
         }
-        void SetState(bool state){
-            state_=state;
+        void SetState(bool state) {
+            state_ = state;
         }
-        void AttachInputs(Component * device){device=nullptr;}
-        void Evaluate() override{
-            for (size_t i=0; i<outputs_.size(); i++){
+        void AttachInputs(Component * device) { device=nullptr; }
+        void Evaluate() override {
+            for (size_t i = 0; i < outputs_.size(); i++) {
                 outputs_[i]->Evaluate();
             }
         }
