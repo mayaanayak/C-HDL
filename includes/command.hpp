f0f0f1ccde706ebb131a137f0d4e460524992f84
Maya Nayak <mayan3@illinois.edu>
2021-12-02 21:30:17 -0600

    Added SeparateByDel() to the header file.

--- includes/command.hpp
+++ includes/command.hpp
@@ -15,6 +15,7 @@ enum command{ HELP, SAVE, LOAD, ADD, WIRE, UNWIRE, DELETE, LIST, RUN, UNKNOWN };
 command GetCommand(const string &argument);
 
 void ToNewFile(const string& file_name);
+vector<string> SeparateByDel(const string& line, char del);
 string ConnectionsToString();
 string KeysToString(const map<string, Component*>& kMap);
 string SchematicKeysToString();
